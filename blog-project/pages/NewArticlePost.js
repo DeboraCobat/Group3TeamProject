4b1f0fa8ef25cd9a46570b4efad67dab5de67f79
Debora Bastos <deboracobat@gmail.com>
2023-06-08 15:16:10 -0400

    fixed home page - no new post yet

---
+++ blog-project/pages/NewArticlePost.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from 'components/NavBar';
+import NewArticleForm from 'components/NewArticleForm';
+
+const NewArticlePost = () => {
+  return (
+    <>
+      <NavBar />
+      <NewArticleForm />
+    </>
+  );
+};
+
+export default NewArticlePost;
--- blog-project/pages/index.js
+++ blog-project/pages/index.js
@@ -1,10 +1,9 @@
 import React from 'react';
-import NavBar from './components/NavBar';
-import ArticlesList from './components/ArticlesList';
-import Footer from './components/Footer';
+import NavBar from 'components/NavBar';
+import ArticlesList from 'components/ArticlesList';
+import Footer from 'components/Footer';
 
 import 'materialize-css/dist/css/materialize.min.css';
-
 
 const Home = () => {
   return (
