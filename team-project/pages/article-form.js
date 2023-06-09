3c39f0a74a7fe58b48ac8cb3ac719b37a7cd85a5
Debora Bastos <deboracobat@gmail.com>
2023-06-09 16:27:29 -0400

    minor css

--- team-project/pages/article-form.js
+++ team-project/pages/article-form.js
@@ -2,13 +2,17 @@ import React from "react";
 import ArticleForm from "pages/components/AddArticleForm";
 import NavBar from "pages/components/NavBar";
 
-const ArticleFormWithNavbar = () => {
+const ArticleFormPage = () => {
   return (
-    <div>
-        <NavBar />
+    <div className="articles-list-container">
+      <NavBar />
+      <div className="article-container">
+        <h3>Let's Create!</h3>
+        <p>Fill out the form below to create a new article.</p>
         <ArticleForm />
+      </div>
     </div>
   );
 };
 
-export default ArticleFormWithNavbar;
+export default ArticleFormPage
