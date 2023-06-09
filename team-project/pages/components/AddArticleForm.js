8ba21eedda7a82ae1c0c77bfdffa741a948537b2
Debora Bastos <deboracobat@gmail.com>
2023-06-09 19:06:06 -0400

    starter on submit article form

--- team-project/pages/components/AddArticleForm.js
+++ team-project/pages/components/AddArticleForm.js
@@ -2,10 +2,13 @@ import React, { useState } from 'react';
 import { db } from 'lib/firebase';
 import styles from 'styles/articleForm.module.css';
 
-
 const ArticleForm = () => {
   const [title, setTitle] = useState('');
   const [content, setContent] = useState('');
+  const [author, setAuthor] = useState('');
+  const [category, setCategory] = useState('');
+  const [coverImage, setCoverImage] = useState('');
+  const [successMessage, setSuccessMessage] = useState('');
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -13,22 +16,31 @@ const ArticleForm = () => {
     const article = {
       title,
       content,
+      author,
+      category,
+      coverImage,
       createdAt: new Date(),
     };
 
     try {
-      await db.collection('articles').add(article);
-      console.log('Article submitted successfully!');
+      const docRef = await db.collection('articles').add(article);
+      console.log('Article submitted successfully! Document ID:', docRef.id);
 
       setTitle('');
       setContent('');
+      setAuthor('');
+      setCategory('');
+      setCoverImage('');
+      setSuccessMessage('Article submitted successfully!');
     } catch (error) {
       console.error('Error submitting article:', error);
+      setSuccessMessage('Error submitting article. Please try again.');
     }
   };
 
   return (
     <form onSubmit={handleSubmit} className={styles['form-container']}>
+      {successMessage && <p>{successMessage}</p>}
       <div className={styles['input-field']}>
         <input
           type="text"
@@ -44,9 +56,33 @@ const ArticleForm = () => {
           placeholder="Enter article content"
         />
       </div>
+      <div className={styles['input-field']}>
+        <input
+          type="text"
+          value={author}
+          onChange={(e) => setAuthor(e.target.value)}
+          placeholder="Enter article author"
+        />
+      </div>
+      <div className={styles['input-field']}>
+        <input
+          type="text"
+          value={category}
+          onChange={(e) => setCategory(e.target.value)}
+          placeholder="Enter article category"
+        />
+      </div>
+      <div className={styles['input-field']}>
+        <input
+          type="text"
+          value={coverImage}
+          onChange={(e) => setCoverImage(e.target.value)}
+          placeholder="Enter article cover image link"
+        />
+      </div>
       <button type="submit" className="btn">Submit</button>
     </form>
-  );  
+  );
 };
 
 export default ArticleForm;
