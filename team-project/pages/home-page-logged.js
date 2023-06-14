2855e7cd126ba3b293c6c8600c46ef20717b861e
Debora Bastos <deboracobat@gmail.com>
2023-06-14 16:10:39 -0400

    css and redesign paths

--- team-project/pages/home-page-logged.js
+++ team-project/pages/home-page-logged.js
@@ -1,27 +1,32 @@
 import Link from 'next/link';
 import React from 'react';
 import styles from '../styles/HomePageLogged.module.css';
+import { useAuth } from '../context/AuthUserContext';
 
 const HomePageLogged = () => {
+  const { authUser } = useAuth();
+
   return (
     <div className={styles.container}>
-      <h1 className={styles.title}>Welcome to your stories home!!</h1>
-      <div>
-        <Link href="/articles-list" passHref>
+      <h1 className={styles.title}>
+        <span className={styles.welcomeMessage}>{`Welcome ${authUser?.email}!`}</span>
+      </h1>
+      <div className={styles.buttonContainer}>
+        <Link href="/articles-list" passHref style={{ textDecoration: 'none' }}>
           <button className={styles.button}>
             See all stories
           </button>
         </Link>
       </div>
-      <div>
-        <Link href="/article-form" passHref>
+      <div className={styles.buttonContainer}>
+        <Link href="/article-form" passHref style={{ textDecoration: 'none' }}>
           <button className={styles.button}>
             Write a new story
           </button>
         </Link>
       </div>
-      <div>
-        <Link href="/user-profile">
+      <div className={styles.buttonContainer}>
+        <Link href="/user-profile" style={{ textDecoration: 'none' }}>
           <div>
             <button className={styles.button}>See my Profile</button>
           </div>
